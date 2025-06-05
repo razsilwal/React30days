7a55995f658b39296725360be4875556867c68da
razsilwal <sabalsilwal51@gmail.com>
2025-06-05 09:04:09 +0545

    learn about useState

--- src/About.jsx
+++ src/About.jsx
@@ -1,7 +1,29 @@
+import { useState } from "react"
 
 function About(){
+    // const state = useState("Raz")
+    // console.log(state)
+    // var count = 0  # we didnt use this so we use useState hooks 
+    
+    // const state = useState(0)
+    // const firstItem = state[0]
+    // const secondItem = state[1]
+    const [number, setNumber] = useState(0)
+
+    const increaseCount = ()=>{
+        setNumber(number + 1)
+    }
+    const decreaseCount = ()=> {
+        if (number > 0){
+        setNumber(number-1)
+        }
+    }
     return(
-        <h1>This is About page</h1>
+        <div>
+            <h1>{number}</h1>
+            <button onClick={increaseCount}>+</button>
+            <button onClick={decreaseCount}>-</button>
+        </div>
     )
 }
 
